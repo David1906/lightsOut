d72ddd639ffb0da90af4a80d4520e499cc53e7e7
David A <david1906@outlook.com>
2020-02-11 21:23:47 -0600

    Updates

---
+++ precache-manifest.d0f08d7ca0eae478ddb6f01f38732d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bb51e0662d9c55e23aa",
+    "url": "/lightsOut/static/js/runtime~main.7bb51e06.js"
+  },
+  {
+    "revision": "8f4252543c4d062db666",
+    "url": "/lightsOut/static/js/main.8f425254.chunk.js"
+  },
+  {
+    "revision": "9b10d1d7aa11e77135f4",
+    "url": "/lightsOut/static/js/1.9b10d1d7.chunk.js"
+  },
+  {
+    "revision": "8f4252543c4d062db666",
+    "url": "/lightsOut/static/css/main.5eb13a79.chunk.css"
+  },
+  {
+    "revision": "960ea19752cc6f8126ee0a65c9bf3abe",
+    "url": "/lightsOut/index.html"
+  }
+];
